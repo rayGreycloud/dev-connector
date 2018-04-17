f7ea76c2e521195fb52e212f54acc268e9fb3d29
rayGreycloud <rayGreycloud@gmail.com>
2018-04-17 11:57:21 -0700

    Set up validation function to check register input

---
+++ validation/register.js
@@ -0,0 +1,28 @@
+const Validator = require('validator');
+
+const isEmpty = require('./is-empty');
+
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+
+  // if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+  //   errors.name = 'Name must be between 2 and 30 characters';
+  // }
+
+  // return {
+  //   errors,
+  //   isValid: isEmpty(errors)
+  // };
+
+  data.name = !isEmpty(data.name) ? data.name : '';
+
+  try {
+    if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+      errors.name = 'Name must be between 2 and 30 characters';
+    }
+  } catch (err) {
+    console.log(err);
+  } finally {
+    return { errors, isValid: isEmpty(errors) };
+  }
+};
