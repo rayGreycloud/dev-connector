ea03dadfa074f2a73c83f755b890f024452b0d04
rayGreycloud <rayGreycloud@gmail.com>
2018-04-17 08:20:17 -0700

    Add passport middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -8,7 +9,7 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
-// Middleware
+// Body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -21,7 +22,9 @@ mongoose
   .then(() => console.log(`MongoDB connected.`))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// Passport - initialize and config
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 // Use routes
 app.use('/api/users', users);
