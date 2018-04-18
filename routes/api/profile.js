ca701b423039da85068947c2e1a16e2eeb1675a5
rayGreycloud <rayGreycloud@gmail.com>
2018-04-18 10:01:24 -0700

    Add post route to create/update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -41,4 +41,71 @@ router.get(
   }
 );
 
+// @route POST api/profile
+// @desc Create user profile route
+// @access Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    // Initialize obj
+    const profileFields = {
+      social: {}
+    };
+
+    // Iterate thru request props
+    for (const prop in req.body) {
+      switch (prop) {
+        // Split skills prop into array
+        case 'skills':
+          profileFields.skills = req.body.skills.split(',');
+          break;
+        // Set social object props
+        case 'twitter':
+        case 'facebook':
+        case 'linkedin':
+        case 'instagram':
+        case 'youtube':
+          profileFields.social[prop] = req.body[prop];
+          break;
+        // Set other profile props
+        default:
+          profileFields[prop] = req.body[prop];
+      }
+    }
+
+    // Look for existing profile
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+    } catch (error) {
+      res.status(404).json(error);
+    }
+
+    // Check if existing profile found
+    if (profile) {
+      // Update existing profile
+      try {
+        const updatedProfile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        res.json(updatedProfile);
+      } catch (error) {
+        res.status(404).json(error);
+      }
+    } else {
+      // Save new profile
+      try {
+        const newProfile = await new Profile(profileFields).save();
+
+        res.json(newProfile);
+      } catch (error) {
+        res.status(404).json(error);
+      }
+    }
+  }
+);
+
 module.exports = router;
