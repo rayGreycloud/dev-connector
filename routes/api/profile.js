d1db997a7dd93d02df056d6b16582a1a8f340caf
rayGreycloud <rayGreycloud@gmail.com>
2018-04-20 17:24:30 -0700

    Add route to get profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -60,6 +60,24 @@ router.get('/handle/:handle', (req, res) => {
     .catch(err => res.status(404).json(err));
 });
 
+// @route GET api/profile/user/:user_id
+// @desc Get profile by user route
+// @access Public
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+  
+  Profile.findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'No profile found for that user';
+        res.status(404).json(errors);
+      }
+      
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json({ profile: 'No profile found for that user id'}));
+});
 
 // @route POST api/profile
 // @desc Create user profile route
