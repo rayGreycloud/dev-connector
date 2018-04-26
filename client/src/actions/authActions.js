b2721f220a6bf74ee1ed8a44da0f3dbfbd768616
rayGreycloud <rayGreycloud@gmail.com>
2018-04-26 08:12:40 -0700

    Add actions to register, login, logout and set current user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,8 +1,64 @@
-import { TEST_DISPATCH } from './types';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken';
 
-export const registeruser = userData => {
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+
+// Register
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    // Send request
+    .post('/api/users/register', userData)
+    // When result received, redirect to login
+    .then(res => history.push('/login'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Login
+export const loginUser = userData => dispatch => {
+  axios
+    // Send request
+    .post('/api/users/login', userData)
+    .then(res => {
+      // Get token
+      const { token } = res.data;
+      // Set token to ls
+      localStorage.setItem('jwtToken', token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
+// Set logged in user
+export const setCurrentUser = decoded => {
   return {
-    type: TEST_DISPATCH,
-    payload: userData
+    type: SET_CURRENT_USER,
+    payload: decoded
   };
+};
+
+// Logout
+export const logoutUser = () => dispatch => {
+  // Remove token from ls
+  localStorage.removeItem('jwtToken');
+  // Remove header
+  setAuthToken(false);
+  // Set current user to {} which sets isAuthenticated to false
+  dispatch(setCurrentUser({}));
 };
