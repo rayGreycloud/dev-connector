5f6a54028a4414c3a2fbdebee71b5143b0222093
rayGreycloud <rayGreycloud@gmail.com>
2018-04-29 14:03:49 -0700

    Add component for social link input group

---
+++ client/src/components/common/InputGroup.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+const InputGroup = ({
+  icon,
+  type,
+  error,
+  placeholder,
+  name,
+  value,
+  onChange
+}) => {
+  return (
+    <div className="input-group mb-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text">
+          <i className={icon} />
+        </span>
+      </div>
+      <input
+        type={type}
+        className={classnames('form-control form-control-lg', {
+          'is-invalid': error
+        })}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
+};
+
+InputGroup.propTypes = {
+  icon: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  error: PropTypes.string,
+  placeholder: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+InputGroup.defaultProps = {
+  type: 'text'
+};
+
+export default InputGroup;
