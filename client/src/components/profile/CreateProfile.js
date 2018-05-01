fdebd2a6fecadb640d530819acb10da16074dc0f
rayGreycloud <rayGreycloud@gmail.com>
2018-05-01 08:54:36 -0700

    Add go back button to return to dashboard

--- client/src/components/profile/CreateProfile.js
+++ client/src/components/profile/CreateProfile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import TextFieldGroup from '../common/TextFieldGroup';
@@ -150,6 +150,9 @@ class CreateProfile extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
+              <Link className="btn btn-light" to="/dashboard">
+                Go Back
+              </Link>
               <h1 className="display-4 text-center">Create Your Profile</h1>
               <p className="lead text-center">
                 Add information about yourself to share on your profile
