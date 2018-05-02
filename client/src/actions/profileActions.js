d417b266097e28ffb27697a2cdfedd8d55ec4e17
rayGreycloud <rayGreycloud@gmail.com>
2018-05-02 13:11:21 -0700

    Add action to get all profiles

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 import {
   GET_PROFILE,
+  GET_PROFILES,
   GET_ERRORS,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
@@ -24,6 +25,26 @@ export const getCurrentProfile = () => dispatch => {
       dispatch({
         type: GET_PROFILE,
         payload: {}
+      })
+    );
+};
+
+// Get profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+
+  axios
+    .get('/api/profile/all')
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
       })
     );
 };
