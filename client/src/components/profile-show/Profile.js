bc849653a4019573e19b12dea474bb1b650e8362
rayGreycloud <rayGreycloud@gmail.com>
2018-05-03 08:43:05 -0700

    Add layout markup and spinner

--- client/src/components/profile-show/Profile.js
+++ client/src/components/profile-show/Profile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import ProfileAbout from './ProfileAbout';
 import ProfileCreds from './ProfileCreds';
@@ -19,18 +19,45 @@ class Profile extends Component {
   }
 
   render() {
+    const { profile, loading } = this.props.profile;
+    let profileContent;
+
+    if (profile === null || loading) {
+      profileContent = <Spinner />;
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link className="btn btn-light mb-3 float-left" to="/profiles">
+                Go Back
+              </Link>
+            </div>
+            <div className="col-md-6" />
+          </div>{' '}
+          {/*  End of Row */}
+          <ProfileHeader profile={profile} />
+          <ProfileAbout />
+          <ProfileCreds />
+          <ProfileGithub />
+        </div>
+      );
+    }
+
     return (
-      <div>
-        <ProfileHeader />
-        <ProfileAbout />
-        <ProfileCreds />
-        <ProfileGithub />
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">{profileContent}</div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 Profile.propTypes = {
+  getProfileByHandle: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired
 };
 
@@ -38,4 +65,4 @@ const mapStateToProps = state => ({
   profile: state.profile
 });
 
-export default connect(mapStateToProps)(Profile);
+export default connect(mapStateToProps, { getProfileByHandle })(Profile);
