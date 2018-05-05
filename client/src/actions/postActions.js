2bec7597bc34f78b36b814eacb257e91821fef91
rayGreycloud <rayGreycloud@gmail.com>
2018-05-05 15:08:52 -0700

    Add actions to set loading state, get posts, like and unlike posts

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,33 +1,13 @@
 import axios from 'axios';
 
 import {
-  POSTS_LOADING,
-  GET_POSTS,
-  GET_POST,
   ADD_POST,
+  GET_POSTS,
+  POST_LOADING,
+  // GET_POST,
   DELETE_POST,
   GET_ERRORS
 } from './types';
-
-// Get posts
-export const getPosts = () => dispatch => {
-  dispatch(setPostLoading());
-
-  axios
-    .get('/api/posts')
-    .then(res =>
-      dispatch({
-        type: GET_POSTS,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: GET_POSTS,
-        payload: null
-      })
-    );
-};
 
 // Create Post
 export const addPost = postData => dispatch => {
@@ -43,6 +23,77 @@ export const addPost = postData => dispatch => {
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
+      })
+    );
+};
+
+// Delete post
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Add like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Remove like
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set loading state
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  };
+};
+
+// Get posts
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading);
+
+  axios
+    .get('/api/posts')
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
       })
     );
 };
