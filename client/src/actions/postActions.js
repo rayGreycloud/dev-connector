fc804856e74112fd828839199d107e9456a87e71
rayGreycloud <rayGreycloud@gmail.com>
2018-05-05 11:35:21 -0700

    Add action to add post

---
+++ client/src/actions/postActions.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+import {
+  POSTS_LOADING,
+  GET_POSTS,
+  GET_POST,
+  ADD_POST,
+  DELETE_POST,
+  GET_ERRORS
+} from './types';
+
+// Get posts
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
+
+  axios
+    .get('/api/posts')
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+// Create Post
+export const addPost = postData => dispatch => {
+  axios
+    .post('/api/posts', postData)
+    .then(res =>
+      dispatch({
+        type: ADD_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
