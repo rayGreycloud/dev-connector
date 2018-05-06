022ee60721d744198a0b1189421a219847fa7c56
rayGreycloud <rayGreycloud@gmail.com>
2018-05-06 12:59:51 -0700

    Add empty keys file

---
+++ config/keys_dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: 'mongodb://admin:admin@ds013926.mlab.com:13926/devconnector',
+  secretOrKey: 'banana',
+  // Not currently consumed; Needed for github repo fetch api
+  githubClientID: 'edc2338f8d4bb3532669',
+  githubClientSecret: '4acad958f3d29b87da14bbea5d901253b626be9f'
+};
