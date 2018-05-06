855c66ed610db5089d049099f2d1226fe9f0561d
rayGreycloud <rayGreycloud@gmail.com>
2018-05-06 06:17:03 -0700

    Move passport to services folder

--- services/passport.js
+++ services/passport.js
@@ -2,7 +2,7 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 
-const keys = require('../config/keys');
+const keys = require('../config/keys_dev');
 const User = mongoose.model('users');
 
 const opts = {};
