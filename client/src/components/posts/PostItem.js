2cc222a7c71e159c835461683249430724fdce4c
rayGreycloud <rayGreycloud@gmail.com>
2018-05-06 05:42:11 -0700

    Implement post like/unlike functionality

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -2,17 +2,37 @@ import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-//import classnames from 'classnames';
+import classnames from 'classnames';
 
-import { deletePost } from '../../actions/postActions';
+import { deletePost, addLike, removeLike } from '../../actions/postActions';
 
 class PostItem extends Component {
   onDeleteClick(id) {
     this.props.deletePost(id);
   }
 
+  onAddLikeClick(id) {
+    this.props.addLike(id);
+  }
+
+  onRemoveLikeClick(id) {
+    this.props.removeLike(id);
+  }
+
+  findUserLike(likes) {
+    const { auth } = this.props;
+
+    return likes.filter(like => like.user === auth.user.id).length > 0;
+  }
+
   render() {
-    const { post, auth } = this.props;
+    const { post, auth, showActions } = this.props;
+
+    const thumbIconClass = classnames({
+      'fas fa-thumbs-up': true,
+      'text-success': this.findUserLike(post.likes),
+      'text-secondary': !this.findUserLike(post.likes)
+    });
 
     return (
       <div className="card card-body mb-3">
@@ -30,24 +50,36 @@ class PostItem extends Component {
           </div>
           <div className="col-md-10">
             <p className="lead">{post.text}</p>
-            <button type="button" className="btn btn-light mr-1">
-              <i className="text-info fas fa-thumbs-up" />
-              <span className="badge badge-light">{post.likes.length}</span>
-            </button>
-            <button type="button" className="btn btn-light mr-1">
-              <i className="text-secondary fas fa-thumbs-down" />
-            </button>
-            <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
-              Comments
-            </Link>
-            {post.user === auth.user.id ? (
-              <button
-                type="button"
-                onClick={this.onDeleteClick.bind(this, post._id)}
-                className="btn btn-danger mr-1"
-              >
-                <i className="fas fa-times" />
-              </button>
+            {showActions ? (
+              <span>
+                <button
+                  onClick={this.onAddLikeClick.bind(this, post._id)}
+                  type="button"
+                  className="btn btn-light mr-1"
+                >
+                  <i className={thumbIconClass} />
+                  <span className="badge badge-light">{post.likes.length}</span>
+                </button>
+                <button
+                  onClick={this.onRemoveLikeClick.bind(this, post._id)}
+                  type="button"
+                  className="btn btn-light mr-1"
+                >
+                  <i className="text-secondary fas fa-thumbs-down" />
+                </button>
+                <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
+                  Comments
+                </Link>
+                {post.user === auth.user.id ? (
+                  <button
+                    type="button"
+                    onClick={this.onDeleteClick.bind(this, post._id)}
+                    className="btn btn-danger mr-1"
+                  >
+                    <i className="fas fa-times" />
+                  </button>
+                ) : null}
+              </span>
             ) : null}
           </div>
         </div>
@@ -56,8 +88,14 @@ class PostItem extends Component {
   }
 }
 
+PostItem.defaultProps = {
+  showActions: true
+};
+
 PostItem.propTypes = {
   deletePost: PropTypes.func.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
   post: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired
 };
@@ -66,4 +104,6 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, { deletePost })(PostItem);
+export default connect(mapStateToProps, { deletePost, addLike, removeLike })(
+  PostItem
+);
